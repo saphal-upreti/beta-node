de2a21e362fe9f3e0cff2766dc22cc1ebc3f3eb2
saphal-upreti <108993335+saphal-upreti@users.noreply.github.com>
2023-11-09 15:33:48 +0545

    homeChanges

--- static/JavaScript/home.js
+++ static/JavaScript/home.js
@@ -1,31 +1,33 @@
-document.getElementById("html").addEventListener(
-  "click",
-  (transit = () => {
-    document.getElementById("svgText").style.fontSize = 340;
-    setTimeout(nextPage, 400);
-  })
-);
-
+window.onload = (event) => {
+  if (document.documentElement.scrollWidth > 900) {
+    document.getElementById("html").addEventListener(
+      "click",
+      (transit = () => {
+        document.getElementById("svgText").style.fontSize = 300;
+        setTimeout(nextPage, 400);
+      })
+    );
+  }
+  if (document.documentElement.scrollWidth < 500) {
+    document.getElementById("html").addEventListener(
+      "click",
+      (transit = () => {
+        document.getElementById("svgText").style.fontSize = 100;
+        setTimeout(nextPage, 400);
+      })
+    );
+  }
+  else {
+    document.getElementById("html").addEventListener(
+      "click",
+      (transit = () => {
+        document.getElementById("svgText").style.fontSize = 150;
+        setTimeout(nextPage, 400);
+      })
+    );
+  }
+};
 nextPage = () => {
   window.open("aboutme", "_self");
 };
 clearInterval(loop());
-
-
-// aboutme=()=>{
-//   // window.open("aboutme", "_self");
-//   console.log("test")
-// }
-
-myPort = () => {
-  window.open("myPort", "_self");
-};
-
-document.getElementsByClassName("myPort").addEventListener(
-  "click",
-  (myPort = () => {
-    window.open("MyPortofolio", "_self");
-  })
-);
-
-
